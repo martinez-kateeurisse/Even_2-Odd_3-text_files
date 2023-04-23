4ad2a7702ba53fc44b76876ac6eb10f7e40a0c9e
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-04-23 18:31:39 +0800

    Pseudocode

---
+++ Even(2)_Odd(3)-text_files.py
@@ -0,0 +1,21 @@
+#Kate Eurisse Martinez_BSCPE 1-5
+
+#A Python program that contains a method that will create two separate text files 
+#after reading the source text file named integers.txt, which contains 20 integers. 
+#The first output file will be named double.txt, containing the square of all even integers found in integers.txt, 
+#and the second file will be named triple.txt, containing the cube of all odd numbers found in integers.txt.
+
+#Create a source text file named integers.txt
+
+#Read and open the text files
+    #For each line
+        #Convert line to int 
+        #If the interger is even
+            #Square the integer
+            #Convert integers to str
+            #Write the result in new text file (double.txt)
+        #if the integer is odd  
+            #Cube the integer
+            #Convert integers to str
+            #Write the result in new text file (triple.txt)
+#Outputs will be printed in new text files
